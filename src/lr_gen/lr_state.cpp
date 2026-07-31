d222b25dacbb6659194dc8168f9f4a071c7a7cec
Max Goren <mgoren@aes-corp.com>
2026-07-31 19:20:52 -0400

    O(nxm) ->O(n)

--- src/lr_gen/lr_state.cpp
+++ src/lr_gen/lr_state.cpp
@@ -4,50 +4,48 @@ LRState::LRState() : state_num(-1), re_calc_key(true) {
 
 }
 
-string LRState::key() const {
-    if (re_calc_key) {
-        set<string> strs;
-        set<string> item_keys;
-        for (const auto& item : items) {
-            strs.insert(item.toString());
-            item_keys.insert(to_string(item.getProduction().pid) + "@" + to_string(item.getDotPosition()));
-        }
-        string result;
-        string cresult;
-        auto rit = strs.begin();
-        auto cit  = item_keys.begin();
-        while (rit != strs.end() && cit != item_keys.end()) {
-            result += *rit + "\n";
-            cresult += *cit + "|";
-            rit++;
-            cit++;
-        }
-        cached_key = result;
-        cached_core = cresult;
-        re_calc_key = false;
-    }
+string& LRState::key() const {
+    if (re_calc_key) update_cache();
     return cached_key;
 }
 
-string LRState::coreKey() const {
-    if (re_calc_key) key();
+string& LRState::coreKey() const {
+    if (re_calc_key) update_cache();
     return cached_core;
+}
+
+void LRState::update_cache() const {
+    if (re_calc_key == false)
+        return;
+    set<string> strs;
+    set<string> item_keys;
+    for (const auto& item : items) {
+        strs.insert(item.toString());
+        item_keys.insert(item.kernel());
+    }
+    cached_key.clear();
+    cached_core.clear();
+    for (auto rit : strs) {
+        cached_key += rit + "\n";
+    }
+    for (auto cit : item_keys) {
+        cached_core += cit + "|";
+    }
+    re_calc_key = false;
 }
 
 bool LRState::mergeLookaheadsFrom(const LRState& other) {
     bool changed = false;
     for (const LRItem& incomingItem : other.items) {
-        auto existing = find_if(items.begin(), items.end(), [&](const LRItem& lri) { return lri.kernel() == incomingItem.kernel(); });
-        if (existing != items.end()) {
-            LRItem existingItem = *existing;
-            items.erase(existing);
-            size_t oldSize = existingItem.lookaheads().size();
-            existingItem.lookaheads().insert(incomingItem.lookaheads().begin(), incomingItem.lookaheads().end());
-            if (existingItem.lookaheads().size() > oldSize) {
+        if (bykern.find(incomingItem.kernel()) != bykern.end()) {
+            int idx = bykern[incomingItem.kernel()];
+            size_t oldSize = items[idx].lookaheads().size();
+            items[idx].lookaheads().insert(incomingItem.lookaheads().begin(), incomingItem.lookaheads().end());
+            if (items[idx].lookaheads().size() > oldSize) {
                 changed = true;
-                existingItem.rehash();
+                items[idx].rehash();
+                re_calc_key = true;
             }
-            items.insert(existingItem);
         }
     }
     return changed;
@@ -62,18 +60,19 @@ void LRState::setStateNum(int sn) {
 }
 
 bool LRState::hasItem(const LRItem& item) {
-    return items.find(item) != items.end();
+    return find(items.begin(), items.end(), item) != items.end();
 } 
 
 void LRState::addItem(LRItem item) {
-    items.insert(item);
+    items.push_back(item);
+    bykern[item.kernel()] = items.size() - 1;
     re_calc_key = true;
 }
 
-unordered_set<LRItem> LRState::getItems() const {
+vector<LRItem>& LRState::getItems() const {
     return items;
 }
-unordered_set<LRItem>& LRState::mutableItems() {
+vector<LRItem>& LRState::mutableItems() {
     return items;
 }
 
--- src/lr_gen/lr_state.hpp
+++ src/lr_gen/lr_state.hpp
@@ -9,21 +9,23 @@ using namespace std;
 class LRState {
     private:
         int state_num;
-        mutable unordered_set<LRItem> items;
+        mutable vector<LRItem> items;
+        mutable unordered_map<string, int> bykern;
         mutable string cached_key;
         mutable string cached_core;
         mutable bool re_calc_key;
     public:
         LRState();
-        string key() const;
-        string coreKey() const;
+        string& key() const;
+        string& coreKey() const;
+        void update_cache() const;
         bool mergeLookaheadsFrom(const LRState& other);
         int getStateNum();
         void setStateNum(int sn);
         bool hasItem(const LRItem& item);
         void addItem(LRItem item);
-        unordered_set<LRItem> getItems() const;
-        unordered_set<LRItem>& mutableItems();
+        vector<LRItem>& getItems() const;
+        vector<LRItem>& mutableItems();
         bool operator==(const LRState& other) const;
         bool operator!=(const LRState& other) const;
 };
