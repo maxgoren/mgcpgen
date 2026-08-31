07e9f66807f74d5d8f24f6ae2d0590389c038aad
Max Goren <mgoren@aes-corp.com>
2026-08-31 18:33:29 -0400

    update examples

--- ex/tiger/tigerEx.sh
+++
@@ -1,6 +0,0 @@
-mgclex grammars/lex_specs/tiger.mlex lex/lexer_matrix.h -c
-mgcpgen -l grammars/tiger.mgrm
-mv mgcpgen_out.hpp parse/
-cp parse/actions_tiger.hpp parse/actions.hpp
-g++ -g repl.cpp -o repl
-./repl example.tig
